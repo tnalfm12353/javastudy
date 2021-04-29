518ef8b9e2f34f0facfad14dbc5db28acaaa2f20
BIT <BIT@DESKTOP-0H6HNMT>
2021-04-29 10:14:53 +0900

    객체지향 프로그래밍 - upCasting, downCasting

--- chapter03/src/main/java/chapter03/Person.java
+++ chapter03/src/main/java/chapter03/Person.java
@@ -2,7 +2,14 @@ package chapter03;
 
 public class Person {
 
-	public Person() {
-		System.out.println("Person() called");
+	private String name;
+
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
