c42cf0aee8ea0f0c0f1dc5b9f8378e3233aae345
BIT <BIT@DESKTOP-0H6HNMT>
2021-05-06 11:01:34 +0900

    httpd #1 작성

---
+++ network/src/main/java/httpd/RequestHandler.java
@@ -0,0 +1,76 @@
+package httpd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class RequestHandler extends Thread {
+	private Socket socket;
+	
+	public RequestHandler( Socket socket ) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			// get IOStream
+			OutputStream outputStream = socket.getOutputStream();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+
+			// logging Remote Host IP Address & Port
+			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
+			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
+			String request = null;
+			
+			while(true) {
+				String line = br.readLine();
+				
+				//브라우져가 연결을 끊으면
+				if(line == null) {
+					break ;
+				}
+				
+				// Request header만 읽음.
+				if("".equals(line)) {
+					break;
+				}
+
+				// 첫 번째 라인만 처리
+				if(request == null) {
+					request = line;
+					break ;
+				}
+			}
+			consoleLog(request);
+			
+			// 예제 응답입니다.
+			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
+			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
+			outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
+			outputStream.write( "\r\n".getBytes() );
+			outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
+
+		} catch( Exception ex ) {
+			consoleLog( "error:" + ex );
+		} finally {
+			// clean-up
+			try{
+				if( socket != null && socket.isClosed() == false ) {
+					socket.close();
+				}
+				
+			} catch( IOException ex ) {
+				consoleLog( "error:" + ex );
+			}
+		}			
+	}
+
+	public void consoleLog( String message ) {
+		System.out.println( "[RequestHandler#" + getId() + "] " + message );
+	}
+}
